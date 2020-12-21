b8bbc4be44870ff5e014cc2ffc20624c07edc23b
Brianne <brianne.ewell1@gmail.com>
2020-12-20 19:03:41 -0700

    fixing issues with delete

--- public/javascript/delete.js
+++ public/javascript/delete.js
@@ -4,7 +4,7 @@ const deletePostHandler = async function(event) {
     event.preventDefault();
 
     fetch("/api/post/" + postId.value, {
-        method: "delete",
+        method: "delete"
     })
     .then(function(){
         document.location.replace("/dashboard");
@@ -12,4 +12,4 @@ const deletePostHandler = async function(event) {
     .catch(err => console.log(err))
 }
 
-document.querySelector("#delete-btn").addEventListener("button", deletePostHandler)
+document.addEventListener('#delete-btn', deletePostHandler)
